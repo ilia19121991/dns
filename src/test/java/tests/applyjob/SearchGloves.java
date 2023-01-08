9cefabde75ddd78e7326afef8786fd1c34458527
Ilia <ilia19011991@gmail.com>
2023-01-08 19:13:11 +0300

    added searchByCatalogTest

--- src/test/java/tests/applyjob/SearchGloves.java
+++ src/test/java/tests/applyjob/SearchGloves.java
@@ -1,13 +1,15 @@
 package tests.applyjob;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class SearchGloves {
@@ -60,22 +62,39 @@ public class SearchGloves {
         });
     }
 
-    @DisplayName("Проверка корректной работы поиска магазинов")
+
+    @DisplayName("Поиск через кнопку Каталог на Главной странице (пока что фэйлится")
     @Test
-    void seacrhShopsTest() {
+    void searchByCatalogTest() {
+        Configuration.timeout = 15000;
+        Selenide.clearBrowserCookies();
+        Configuration.holdBrowserOpen = true;
+
         step("Открываем сайт главную страницу DNS", () -> {
             open("https://www.dns-shop.ru/");
         });
-        step("Выбираем пункт 'Магазины'", () -> {
-            $(".header-top-menu").find(byText("Магазины")).click();
+        step("Подтверждаем свой город", () -> {
+            $(".v-confirm-city__buttons_qv9").find(byText("Всё верно")).click();
         });
-        step("Выбиваем с строку поиска по магазинам 'белая дача'", () -> {
-            $("[data-role=shop-search-field]").setValue("белая дача");
+        step("Нажимаем на кнопку Каталог", () -> {
+            $("[data-role=catalog-button]").click();
         });
-        step("Проверяем, что в выдаче есть магазин DNS в Мега Белая Дача", () -> {
-            $(".shop-list-item:not(.hidden)").shouldHave(text("белая дача"));
+        step("Наводим мышку на пункт: ПК, ноутбуки, периферия", () -> {
+            $(".header-menu-desktop-wrap").find(byText("ПК, ноутбуки, периферия")).click();
         });
+        step("Выбираем пункт: Ноутбуки ", () -> {
+            $(byXpath("//*[@id=\"header-search\"]/div/div[4]/div/div[2]/div/div[2]/div[1]/a[1]")).click();
+          //  $(".header-menu-desktop__second-level-wrap").find(byLinkText("noutbuki")).click();
+        });
+        step("Выбираем раздел Ноутбуки и аксессуары", () -> {
+            $(".subcategory__item-container").find(byText("Ноутбуки и аксессуары")).click();
+        });
+
+        step("Проверяем, что в первом элементе списка выдачи находится ноутбук", () -> {
+            $$("[data-id=product]").first().shouldHave(text("ноутбук"));
+        });
+
+
 
     }
-
 }
